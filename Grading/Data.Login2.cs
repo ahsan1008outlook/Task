fdb92cbfe722c3d7c890d6c052820fcc53bbda30
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-02-25 16:14:37 +0500

    GO

--- Grading/Data.Login2.cs
+++ Grading/Data.Login2.cs
@@ -17,46 +17,46 @@ public static partial class Data
         if (result) return result;
 
         List<Employee> employeesNew = new List<Employee>();
-        double lastSkip = 0; // 0, 3.8, 7.6, 11.4, 15.2, 19, 22.8, 26.6
+        double lastSkip = 0;
         double carryForward = 0;
         for (int i = 0; i < totalGrades; i++)
         {
             if (grades[i].PercentageToAssign == 0) continue;
-
             double percentage = grades[i].PercentageToAssign / 100;
             double distribution = percentage * totalEmployees;
-            // 1.5,    3    2.25    1.5     1.5     2.25    1.5     .75     .75
-            // -.5,    -.5  -.25    -.75    -1.5    -1.25   -2.25   
-            // 2,      3    2        
+
             int toTaken = ConvertToIntReq(distribution + carryForward);
             int toSkip = ConvertToIntReq(lastSkip);
 
             if (toTaken >= 1)
             {
                 var setOfEmployee = employees.Skip(toSkip).Take(toTaken);
-                Console.WriteLine("Take {0} Skip {1} LastSkip {2} Per% {3} Dist {4} carryForward {5}", toTaken, toSkip, lastSkip, percentage, distribution, carryForward);
-
                 foreach (var emp in setOfEmployee)
                 {
                     emp.Grade = grades[i].Name;
                     emp.GradeRank = grades[i].Rank;
                 }
                 employeesNew.AddRange(setOfEmployee);
+                // Console.WriteLine("Take {0} Skip {1} LastSkip {2} Per% {3} Dist {4} carryForward {5}", toTaken, toSkip, lastSkip, percentage, distribution, carryForward);
+
             }
-            if(toTaken > distribution)
-            carryForward += distribution - toTaken;
-            else if(toTaken < distribution)
-            carryForward += distribution - toTaken;
+            // Handle Carry Forward Decimals
+            {
+                if (toTaken > distribution)
+                    carryForward += distribution - toTaken;
+                else if (toTaken < distribution)
+                    carryForward += distribution - toTaken;
+            }
+
 
             lastSkip += distribution;
         }
-        Console.WriteLine("Total " + lastSkip);
 
-        // Print the results
         foreach (var emp in employeesNew)
         {
             Console.WriteLine($"{emp.Name} - Grade: {emp.Grade}, Rank: {emp.GradeRank}");
         }
+        Console.WriteLine("Total " + lastSkip);
         Console.WriteLine("Total " + employeesNew.Count);
         return true;
     }
